79de1432044495d2f448d61d20e681d31c40ed3b
cntrvsy <benintangana@gmail.com>
2024-10-24 14:48:24 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { goto } from "$app/navigation";
+   </script>
+   
+   <div class="grid grid-cols-3 gap-4 p-4">
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn btn-primary" onclick={() => goto('/counter')}>Counter</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+       <div class="p-4 flex items-center justify-center">
+           <button class="btn">First Example</button>
+       </div>
+   </div>
