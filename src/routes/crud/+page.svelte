47413572bf2dd2cfa3f401dcab80ce631d418ab7
cntrvsy <benintangana@gmail.com>
2025-03-03 01:06:29 +0300

    CRUD

--- src/routes/crud/+page.svelte
+++ src/routes/crud/+page.svelte
@@ -17,42 +17,89 @@
         surname: ''
     })
 
+    let prefix = $state('')
+    const filteredPeople = $derived(
+        prefix ? people.filter(p => p.name.toLowerCase().startsWith(prefix)) : people
+    )
+
+    function createPerson() {
+        people = [...people, person]
+        clearFields()
+    }
+
+    function updatePerson() {
+        const index = people.indexOf(selected!)
+        people[index] = person
+    }
+
+    function deletePerson() {
+        people = people.filter(p => p !== selected)
+    }
+
+    function clearFields() {
+        person = {
+            name: '',
+            surname: ''
+        }
+    }
+    $effect(() => {
+        person = {
+            name: selected?.name ?? '',
+            surname: selected?.surname ?? ''
+        }
+    })
 </script>
 
 <div class="flex items-center justify-center h-screen">
-    <div class="flex flex-col items-center space-y-4">
+    <!-- columns -->
+    <div class="grid grid-cols-2 gap-4">
+        <div>
+            <!-- column 1 -->
+            <div class="search">
+                <label class="group">
+                    <span>
+                        Filter prefix:
+                    </span>
+                    <input type="text" class="input input-bordered" bind:value={prefix}/>
+                </label>
+            </div>
 
-        <div class="search">
-            <label class="group">
-                <span>
-                    Filter prefix:
+            <div class="py-4">
+                <span class="group">
+                    Names:
                 </span>
-                <input type="text" class="input input-bordered"/>
-            </label>
+                
+                <select bind:value={selected} class="select select-bordered w-full h-auto" size="6">
+                    {#each filteredPeople as person}
+                        <option value={person}>
+                            {person.name} {person.surname}
+                        </option>
+                    {/each}
+                </select>
+                
+            </div>
         </div>
 
-        <label class="people">
-            <span class="group">
-                Names:
-            </span>
-            <select bind:value={selected} size="5" class="select select-bordered w-full">
-                {#each people as person}
-                    <option value={person}>
-                        {person.name} {person.surname}
-                    </option>
-                {/each}
-            </select>
-        </label>
-
-        <div class="details">
-            <label class="group">
-                <span>Name:</span>
-                <input type="text" bind:value={person.name} class="input input-bordered"/>
-            </label>
-            <label class="group">
-                <span>Surname:</span>
-                <input type="text" bind:value={person.surname} class="input input-bordered"/>
-            </label>
+        <div class="grid grid-cols-1 place-items-center">
+            <!-- column 2 -->
+            <div class="">
+                <div class="py-2">
+                    <span class="pr-5">Name:</span>
+                    <input type="text" bind:value={person.name} class="input input-bordered"/>
+                </div>
+                <div class="py-2">
+                    <span>Surname:</span>
+                    <input type="text" bind:value={person.surname} class="input input-bordered"/>
+                </div>
+            </div>
+        </div>
+        <!-- nav buttons -->
+        <div>
+            <div class="flex justify-between">
+                <button class="btn btn-primary" disabled={!person.name || !person.surname} onclick={createPerson}>Create</button>
+                <button class="btn btn-primary" disabled={!selected} onclick={updatePerson}>Update</button>
+                <button class="btn btn-primary" disabled={!selected} onclick={deletePerson}>Delete</button>
+            </div>
         </div>
     </div> 
 </div>
